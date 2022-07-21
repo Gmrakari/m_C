ba33a139f07f63a99599f094333add333fcbea4f
Gmrakari <gmrakari@outlook.com>
2022-07-21 09:06:15 +0800

    [Update] add between function and function a blank line

--- POINTERS ON C/ch01/stdin_stdout_program1.c
+++ POINTERS ON C/ch01/stdin_stdout_program1.c
@@ -44,6 +44,7 @@ int read_column_numbers(int columns[], int max) {
     return num;
 
 }
+
 void rearrange(char *output, char const *input, int n_columns, int const columns[]) {
     int col;
     int output_col;
