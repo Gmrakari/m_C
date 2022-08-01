0ae51cdfb12321403ac0027b7257409a107626ca
Gmrakari <gmrakari@outlook.com>
2022-08-01 15:52:28 +0800

    [Update]增加可读性注释

--- Cstring/fread_fopen.c
+++ Cstring/fread_fopen.c
@@ -5,6 +5,24 @@
 
 static int cnt = 0;
 
+/*
+** FILE *fopen(const char *path, const char *mode);
+** parm:mode字符串则代表着流形态
+**
+** r 打开只读文件，该文件必须存在。
+** r+ 打开可读写的文件，该文件必须存在。
+** w 打开只写文件，若文件存在则文件长度清为0，即该文件内容会消失。若文件不存在则建立该文件。
+** w+ 打开可读写文件，若文件存在则文件长度清为零，即该文件内容会消失。若文件不存在则建立该文件。
+**
+*/
+
+/*
+**  @method : fileOp()
+**  @brief  : 文件操作，将文件中写入一个数字，读取文件中的数字
+**  @return : 返回文件中的数字
+**
+*/
+
 void fileOp()
 {
     char filepath[] = "/home/gmrakari/github/m_C/Cstring/cnt_mtqq_lost_connfiles";
