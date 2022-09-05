033603cdd6d33c5640e69cbd2ccc5ab553c203c9
Gmrakari <gmrakari@outlook.com>
2022-09-05 16:22:36 +0800

    [Update] add delBlank return string by book

--- POINTERS ON C/ch04/solution/07/src/delBlankChar.c
+++ POINTERS ON C/ch04/solution/07/src/delBlankChar.c
@@ -35,10 +35,42 @@ void test()
 {
     char str[] = "            ab     c d  e         f ";
     printf("%s\n", str);
-
     deblank(str);
-
     printf("%s\n", str);
+
+    char str2[] = "            ab     c d  e         f ";
+    printf("%s\n", str2);
+    deblank_book(str2);
+    printf("%s\n", str2);
+}
+
+void deblank_book(char *string)
+{
+    char *dest;
+    char *src;
+    int ch;
+
+    src = string;
+    dest = string++;
+
+    while ((ch = *src++) != NULL)
+    {
+        if (is_blank(ch))
+        {
+            if (src == string || !is_blank(dest[-1]))
+                *dest++ = ' ';
+        }
+        else
+        {
+            *dest++ = ch;
+        }
+    }
+    *dest = NULL;
+}
+
+int is_blank(int ch)
+{
+    return ch == ' ' || ch == '\t' || ch == '\v' || ch == '\f' || ch == '\n' || ch == '\r';
 }
 
 int main()
