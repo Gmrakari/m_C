644c05c643e49fc11de0fe74ebdf49e05eb82002
Gmrakari <gmrakari@outlook.com>
2023-02-02 09:47:52 +0800

    [ADD] left op

---
+++ tmpTest/leftOp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int calc_char() {
+    char uid[] = "001122334455667";
+}
+
+int main(int argc, char const *argv[]) {
+    int i = 1;
+    i <<= 1;
+    printf("i:%d\n", i);
+
+        i <<= 1;
+    printf("i:%d\n", i);
+}
