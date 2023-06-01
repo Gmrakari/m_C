74fe2762b3e2c86f0520fc2dda2ade2f1e7b6009
Gmrakari <gmrakari@outlook.com>
2023-06-01 20:41:47 +0800

    [add] null ptr op err test

---
+++ basic/pointer/null_pointer_op_result/ptr_null_op.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main() {
+    printf("1abc\r\n");
+
+    char *ptr = NULL;
+
+    // ptr = "asda";
+
+    // printf("ptr:%s\r\n", ptr);
+
+
+    // ptr = NULL;
+
+    ptr += 1;
+
+    printf("ptr:%s\r\n", ptr);
+
+
+    printf("3abc\r\n");
+}
