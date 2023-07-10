47955fcb5730ee53471fd5a23356a533f2c1e431
Gmrakari <gmrakari@outlook.com>
2023-07-10 10:55:16 +0800

    [update] calc h*w*b and compare jpg file size different

--- IMG_Handle/JPEG/01_jpg_data_format/read_jpg_img.c
+++ IMG_Handle/JPEG/01_jpg_data_format/read_jpg_img.c
@@ -6,7 +6,10 @@ typedef struct {
     uint32_t width;
     uint32_t height;
     uint32_t bpp;
+    size_t jpg_size;
 } jpg_img_info_t;
+
+#define LOGI(format, ...) printf("(%d):"format, __LINE__, ##__VA_ARGS__)
 
 static void _get_jpg_width_and_height(const char *filename, jpg_img_info_t *info)
 {
@@ -14,6 +17,15 @@ static void _get_jpg_width_and_height(const char *filename, jpg_img_info_t *info
 
     if (NULL == (fp = fopen(filename, "r+")))
         return ;
+
+    size_t file_end, file_start, file_len;
+
+    fseek(fp, 0, SEEK_END);
+    file_end = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    file_start = ftell(fp);
+    file_len = file_end - file_start;
+    info->jpg_size = file_len;
 
     // 1.初始化 libjpeg 解码器
     struct jpeg_decompress_struct cinfo;
@@ -46,9 +58,15 @@ static void _print_jpg_width_and_height(jpg_img_info_t *info)
     if (info == NULL)
         return ;
 
-    printf("jpg height: %d\r\n", info->height);
-    printf("jpg width: %d\r\n", info->width);
-    printf("jpg bpp: %d\r\n", info->bpp);
+    LOGI("jpg height:%d\r\n", info->height);
+    LOGI("jpg width:%d\r\n", info->width);
+    LOGI("jpg bpp:%d\r\n", info->bpp);
+
+    // JPEG文件的大小和使用libjpeg计算出来的图像宽度、高度以及位深度（bpp）之间没有直接的关系 
+    // 1.JPEG文件的大小只表示压缩后的数据量
+    // 2.图像的宽度、高度和位深度则是解码后的图像属性
+    LOGI("jpg height * width * bpp size:%d\r\n", info->height * info->width * info->bpp);
+    LOGI("jpg file size:%d\r\n", (int)info->jpg_size);
 }
 
 static void test_case()
