25311fc7275ef30653b2aa96bcbf7ca06bf0ef92
Gmrakari <gmrakari@outlook.com>
2023-07-13 10:12:59 +0800

    [update] remove redefine headers
    [update] use macro replace printf in app
    
    [update] update var data type

--- basic/c_time_op/timestamp_2_struct_tm.c
+++ basic/c_time_op/timestamp_2_struct_tm.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h> // malloc
-
-#include <bits/types.h>  // time_t
-#include <time.h>        // time_t
+#include <stdlib.h>    // malloc
+#include <time.h>      // time_t
+#include <stdint.h>    // uint8/16/32/64...
 
 #define SEC_PER_MINUTE           (60)
 #define SEC_PER_HOUR             (3600)
 #define SEC_PER_DAY              (86400)
 #define SEC_PER_YEAR             (31536000)
+
+#define LOGI(format, ...) printf(format"\r\n" , ##__VA_ARGS__)
 
 // 时间戳，转换成tm结构体的时间，用完需要释放内存
 static struct tm *timestamp_2_tm(const time_t timestamp)
@@ -48,7 +49,7 @@ static struct tm *timestamp_2_tm(const time_t timestamp)
     }
 
     // 计算月份
-    int month_lengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int16_t month_lengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if ((cur_tm->tm_year % 4 == 0 && cur_tm->tm_year %100 != 0) || cur_tm->tm_year % 400 == 0)
         month_lengths[1] = 29;
 
@@ -70,12 +71,12 @@ static struct tm *timestamp_2_tm(const time_t timestamp)
     cur_tm->tm_sec = seconds % SEC_PER_MINUTE;
 
     // 使用Zellers Kongruenz 公式计算任何一日属一星期中哪一日
-    int year_of_century = cur_tm->tm_year % 100;
-    int century = cur_tm->tm_year / 100;
+    int16_t year_of_century = cur_tm->tm_year % 100;
+    int16_t century = cur_tm->tm_year / 100;
 
-    int month = cur_tm->tm_mon + 1;
-    int day = cur_tm->tm_mday;
-    int weekday = -1;
+    int16_t month = cur_tm->tm_mon + 1;
+    int16_t day = cur_tm->tm_mday;
+    int16_t weekday = -1;
 
     if (month == 1 || month == 2) {
         year_of_century --;
@@ -88,7 +89,7 @@ static struct tm *timestamp_2_tm(const time_t timestamp)
     cur_tm->tm_wday = weekday % 7;
 
     // 计算一年中的日期 [0,365], 其中0表示1月1日
-    int year_day = 0;
+    int16_t year_day = 0;
     for (int i = 0; i < cur_tm->tm_mon; i++) {
         year_day += month_lengths[i];
     }
@@ -103,12 +104,14 @@ static struct tm *timestamp_2_tm(const time_t timestamp)
 
 static void print_tm(struct tm *cur_st_time)
 {
-    printf("%04d-%02d-%02d %02d:%02d:%02d\r\n", cur_st_time->tm_year, cur_st_time->tm_mon + 1, cur_st_time->tm_mday,
+    LOGI("%04d-%02d-%02d %02d:%02d:%02d", cur_st_time->tm_year, cur_st_time->tm_mon + 1, cur_st_time->tm_mday,
                                                 cur_st_time->tm_hour, cur_st_time->tm_min, cur_st_time->tm_sec);
-    printf("tm_wday:%d\r\n", cur_st_time->tm_wday);
-    printf("tm_yday:%d\r\n", cur_st_time->tm_yday);
-    printf("tm_isdst:%d\r\n", cur_st_time->tm_isdst);
-    printf("\r\n");
+    LOGI("tm_wday:%d", cur_st_time->tm_wday);
+    LOGI("tm_yday:%d", cur_st_time->tm_yday);
+    LOGI("tm_isdst:%d", cur_st_time->tm_isdst);
+
+    LOGI("tm_wday:%d", cur_st_time->tm_wday);
+    LOGI("");
 }
 
 static void testcase()
