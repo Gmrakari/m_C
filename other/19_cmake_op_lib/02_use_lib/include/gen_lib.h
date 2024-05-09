6d82f8ccb4e16876859be8c2fd37bb164fd200f8
gmrakari <gmrakari@outlook.com>
2024-05-09 18:04:57 +0800

    [add] use static / shared lib

---
+++ other/19_cmake_op_lib/02_use_lib/include/gen_lib.h
@@ -0,0 +1,8 @@
+#ifndef _GEN_LIB_H_
+#define _GEN_LIB_H_
+
+#include <stdio.h>
+
+void _print_gen_lib(void);
+
+#endif // _GEN_LIB_H_
---
+++ other/19_cmake_op_lib/02_use_lib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "gen_lib.h"
+
+int main() 
+{
+    _print_gen_lib();
+
+    return 0;
+}
