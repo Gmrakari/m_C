005cb34541a3708b769a5a5941202fe48d0b5877
gmrakari <gmrakari@outlook.com>
2024-05-09 18:03:38 +0800

    [add] generate static / shared lib demo

---
+++ other/19_cmake_op_lib/01_gen_lib/main.c
@@ -0,0 +1,9 @@
+#include "gen_lib.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    _print_gen_lib();
+
+    return 0;
+}
---
+++ other/19_cmake_op_lib/01_gen_lib/src/gen_lib.c
@@ -0,0 +1,8 @@
+#include "gen_lib.h"
+
+#include <stdio.h>
+
+void _print_gen_lib(void)
+{
+    printf("[%s][%d]\r\n", __func__, __LINE__);
+}
