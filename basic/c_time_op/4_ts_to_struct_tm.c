8cbb3f2f6cea00176eb0c7c1b4e1268f345b2137
gmrakari <gmrakari@outlook.com>
2024-08-27 18:17:21 +0800

    add localtime usage

--- basic/c_time_op/4_ts_to_struct_tm.c
+++ basic/c_time_op/4_ts_to_struct_tm.c
@@ -15,6 +15,7 @@ static void _test_1()
     info = localtime(&rawtime);
     if (info) {
         strftime(buffer, sizeof(buffer), "%b %d %H:%M:%S", info);
+        printf("[%s][%d]buffer:%s\r\n", __func__, __LINE__, buffer);
     }
 
     int offset_hours = info->tm_gmtoff / 3600;
@@ -33,7 +34,32 @@ static void _test_1()
     printf("%s\r\n", buffer);
 }
 
+static int _localtime_usage(void)
+{
+    int ret = 0;
+
+    time_t rawtime;
+
+    struct tm *info;
+
+    time( &rawtime );
+
+    info = localtime(&rawtime);
+
+    char buffer[32] = {0};
+
+    if (info) {
+        strftime(buffer, sizeof(buffer), "%b %d %H:%M:%S", info);
+        printf("[%s][%d]buffer:%s\r\n", __func__, __LINE__, buffer);
+    
+        printf("current local time :\r\n %s", asctime(info));
+    }
+
+    return ret;
+}
+
 int main()
 {
-    _test_1();
+    _localtime_usage();
+    // _test_1();
 }
