2aae4a08c3f04454172d1a3d27f6bdeefb8d2132
gmrakari <gmrakari@outlook.com>
2024-10-15 14:51:00 +0800

    update fseek ftell usage demo

--- file_op/6_fseek_ftell.c
+++ file_op/6_fseek_ftell.c
@@ -13,22 +13,24 @@
 // SEEK_CUR 	/* 文件指针当前位置 Seek from current position.  */
 // SEEK_END 	/* 文件末尾 Seek from end of file.  */
 
-static void _fseek_ftell_usage()
+static int _fseek_ftell_usage(const char *filepath, int *olen)
 {
+    if (!filepath || !olen)
+        return -1;
+
     int ret = 0;
-    const char *path = "fseek_ftell_test_file.txt";
+    const char *path = filepath;
 
-    size_t str_len = strlen(path);
-    DEBUG("str_len:%d", (int)str_len);
+    FILE *fp = NULL;
 
-    FILE *fp;
     if (NULL == (fp = fopen(path, "r+"))) {
         DEBUG("open file : %s err!", path);
-        return ;
+        return -1;
     }
 
     if (0 != (ret = fseek(fp, 0, SEEK_END))) {
         DEBUG("fseek err!");
+        return -1;
     }
 
     size_t start = 0, end = 0, file_size = 0;
@@ -36,6 +38,7 @@ static void _fseek_ftell_usage()
 
     if (0 != (ret = fseek(fp, 0, SEEK_SET))) {
         DEBUG("fseek err!");
+        return -1;
     }
     start = ftell(fp);
 
@@ -43,19 +46,91 @@ static void _fseek_ftell_usage()
 
     DEBUG("file_size:%d", (int)file_size);
 
-    if (file_size != str_len) {
-        DEBUG("cal err");
-    }
+    *olen = file_size;
 
-    DEBUG("cal %d", (int)file_size);
+    return ret;
 }
 
-static void _test_app()
+static int _fread_len_usage(const char *filepath, int *olen)
 {
-    _fseek_ftell_usage();
+    if (!filepath || !olen)
+        return -1;
+
+    const char *path = "fseek_ftell_test_file.txt";
+
+    FILE *fp;
+    if (NULL == (fp = fopen(path, "r"))) {
+        DEBUG("open file : %s err!", path);
+        return -1;
+    }
+
+    int buffer[1024] = {0};
+
+    int read_len = 0;
+
+    read_len = fread(buffer, 1, sizeof(buffer), fp);
+
+    fclose(fp);
+    fp = NULL;
+
+    DEBUG("read len: %d", (int)read_len);
+
+    *olen = read_len;
+
+    return 0;
+}
+
+static int _func_get_file_len_1(const char *filepath)
+{
+    if (!filepath)
+        return -1;
+
+    int len = 0;
+    int rc = -1;
+
+    rc = _fseek_ftell_usage(filepath, &len);
+    if (rc != 0)
+        DEBUG("_fseek_ftell_usage err");
+
+    DEBUG("olen: %d", len);
+
+    return rc;
+}
+
+static int _func_get_file_len_2(const char *filepath)
+{
+    if (!filepath)
+        return -1;
+
+    int len = 0;
+    int rc = -1;
+
+    rc = _fread_len_usage(filepath, &len);
+    if (rc != 0)
+        DEBUG("_fread_len_usage err");
+
+    DEBUG("olen: %d", len);
+
+    return rc;
+}
+
+static int _test_func_app(void)
+{
+    int ret = 0;
+    const char filepath[] = "fseek_ftell_test_file.txt";
+
+    DEBUG("test 1");
+
+    ret = _func_get_file_len_1(filepath);
+
+    printf("\r\n");
+    DEBUG("test 2");
+    ret = _func_get_file_len_2(filepath);
+
+    return ret;
 }
 
 int main()
 {
-    _test_app();
+    _test_func_app();
 }
