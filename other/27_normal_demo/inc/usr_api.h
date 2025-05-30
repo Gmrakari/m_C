36b9ae5411076fe10afa9ef44d9b8b37ae61b7e6
gmrakari <gmrakari@outlook.com>
2025-05-30 10:24:50 +0800

    normal demo startup app

---
+++ other/27_normal_demo/inc/usr_api.h
@@ -0,0 +1,6 @@
+#ifndef _USR_API_H_
+#define _USR_API_H_
+
+int usr_api_app(void);
+
+#endif // _USER_HEADER_H
---
+++ other/27_normal_demo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "usr_api.h"
+
+int main()
+{
+    usr_api_app();
+    return 0;
+}
